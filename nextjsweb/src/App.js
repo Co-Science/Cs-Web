5c84dc10f32cbaf61c3158498ba3aa05362d8065
joel <joel2000nickson@gmail.com>
2021-04-11 05:09:39 +0530

    created:Full functional web app"

--- nextjsweb/src/App.js
+++ nextjsweb/src/App.js
@@ -1,23 +1,30 @@
 import './App.css';
 import {Button, Col,Container,Row,Card,Navbar, Carousel,Table} from "react-bootstrap"
-import {showImage} from "./utils/call_img"
+import Iframe from "react-iframe";
+import iframer from "./utils/call_img"
+
 
 function App() {
   return (
     <>
-    <div className="App">
-      <Navbar bg="dark" variant="dark" className='my-nav'>
-        <Navbar.Brand href="#home">
-          S3 | ComputerScience_n_Eng
-        </Navbar.Brand>
-      </Navbar>
+      <div className="App">
+        <Navbar bg="dark" variant="dark" className='my-nav'>
+          <Navbar.Brand href="#home">
+            S3 | ComputerScience_n_Eng
+          </Navbar.Brand>
+        </Navbar>
 
-      <Container>
+        <Container>
+          {/* Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop */}
+          <Row className="row">         
+            {/* <Iframe src="https://drive.google.com/embeddedfolderview?id=11vfkdvA15xTRzQp0vpxwUJr4tDAjPaqr#grid"/>
+            <Iframe src="https://drive.google.com/embeddedfolderview?id=11vfkdvA15xTRzQp0vpxwUJr4tDAjPaqr#grid"/> */}
+            {iframer()}
+          </Row>
+          
+        </Container>
         
-        
-      </Container>
-      
-    </div>
+      </div>
     </>
   );
 }
