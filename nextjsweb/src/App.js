78f773f52f1e16756152244e7a566ed648e54c39
joel <joel2000nickson@gmail.com>
2021-04-11 01:13:39 +0530

    init commit

---
+++ nextjsweb/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import {Button, Col,Container,Row,Card,Navbar, Carousel,Table} from "react-bootstrap"
+import {showImage} from "./utils/call_img"
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <Navbar bg="dark" variant="dark" className='my-nav'>
+        <Navbar.Brand href="#home">
+          S3 | ComputerScience_n_Eng
+        </Navbar.Brand>
+      </Navbar>
+
+      <Container>
+        
+        
+      </Container>
+      
+    </div>
+    </>
+  );
+}
+
+export default App;
